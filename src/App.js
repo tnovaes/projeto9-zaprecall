41aff3de95dac585f23e5a23675144466ff3a7e5
TiagoNovaes <tiagousnovaes@gmail.com>
2023-03-01 11:42:10 -0300

    Versão com inicio do Layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/logo.png'
+import styled from 'styled-components';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tela>
+      <Header>
+        <img src={logo} alt="logo"/>
+        <h1>ZapRecall</h1>
+      </Header>
+      <Container>
+        <div className="Pergunta">
+          Pergunta 1
+        </div>
+        <div className="Pergunta">
+          Pergunta 2
+        </div>
+      </Container>
+      <Footer>
+        0/4 Concluídos
+      </Footer>
+    </Tela>
   );
 }
 
-export default App;
+const Tela = styled.div`
+  width:100vw;
+  height:100vh;
+  display:flex;
+  flex-direction:column;
+  align-items: center;
+  background-color: #FB6B6B;
+`
+
+const Header = styled.div`
+  width:100%;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  position:fixed;
+  top:48px;
+  left:0;
+  gap:20px;
+  img{
+    width:52px;
+    height:60px;
+  }
+  h1{
+    height:44px;
+    font-size:36px;
+  }
+`
+
+const Container = styled.div`
+  height:700px;
+  margin-top:153px;
+`
+
+const Footer = styled.div`
+  width:100vw;
+  height:70px;
+  position:fixed;
+  bottom:0;
+  right:0;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #FFFFFF;
+`
